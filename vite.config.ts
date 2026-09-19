5980d5bfecff87e20a9ad7235ba948b491832233
moritzbrantner <31051084+moritzbrantner@users.noreply.github.com>
2026-09-19 04:32:01 +0200

    Keep Vite config browser-typed

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
 import { defineConfig } from "vite";
 
-export default defineConfig({
-  base: process.env.GITHUB_ACTIONS ? "/viz-engine/" : "/",
+export default defineConfig(({ command }) => ({
+  base: command === "build" ? "/viz-engine/" : "/",
   build: {
     target: "es2022",
   },
-});
+}));
